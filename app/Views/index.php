dd9a6772268c1ebd93ecf22ed936a68b9e8dc4dd
i3rito <50003342+i3rito@users.noreply.github.com>
2020-12-23 00:49:25 -0300

    Reposiciona diretórios

--- app/Views/index.php
+++ app/Views/index.php
@@ -8,29 +8,9 @@
     <meta name="generator" content="Jekyll v4.0.1">
     <title>Jumbotron Template · Bootstrap</title>
 
-    <link rel="canonical" href="https://getbootstrap.com/docs/4.5/examples/jumbotron/">
-
     <!-- Bootstrap core CSS -->
-<link href="../../Bootstrap-4.5.0\css\bootstrap.css" rel="stylesheet">
+    <link href="../src/Bootstrap-4.5.0/css/bootstrap.css" rel="stylesheet">s
 
-    <style>
-      .bd-placeholder-img {
-        font-size: 1.125rem;
-        text-anchor: middle;
-        -webkit-user-select: none;
-        -moz-user-select: none;
-        -ms-user-select: none;
-        user-select: none;
-      }
-
-      @media (min-width: 768px) {
-        .bd-placeholder-img-lg {
-          font-size: 3.5rem;
-        }
-      }
-    </style>
-    <!-- Custom styles for this template -->
-    <link href="../../Bootstrap-4.5.0/jumbotron.css" rel="stylesheet">
   </head>
   <body>
     <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
@@ -69,13 +49,12 @@
 <main role="main">
 
   <!-- Main jumbotron for a primary marketing message or call to action -->
-  <div class="jumbotron">
-    <div class="container">
-      <h1 class="display-3">Hello, world!</h1>
-      <p>This is a template for a simple marketing or informational website. It includes a large callout called a jumbotron and three supporting pieces of content. Use it as a starting point to create something more unique.</p>
-      <p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more &raquo;</a></p>
+    <div class="jumbotron" style="background-image: url('https://upload.wikimedia.org/wikipedia/commons/9/9a/Forum_Jo%C3%A3o_Mendes_Junior.jpg'); background-size: cover; height: 65vh;">
+        <div class="container">
+            <h1 class="display-3">Versation Publicidade</h1>
+            <p>This is a template for a simple marketing or informational website. It includes a large callout called a jumbotron and three supporting pieces of content. Use it as a starting point to create something more unique.</p>
+        </div>
     </div>
-  </div>
 
   <div class="container">
     <!-- Example row of columns -->
@@ -107,5 +86,5 @@
   <p>&copy; Company 2017-2020</p>
 </footer>
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-      <script>window.jQuery || document.write('<script src="../../Bootstrap-4.5.0\\js\\bootstrap.js"><\/script>')</script><script src="../../Bootstrap-4.5.0\js\bootstrap.js"></script></body>
+      <script>window.jQuery || document.write('<script src="../src/Bootstrap-4.5.0/js/bootstrap.js"><\/script>')</script><script src="../src/Bootstrap-4.5.0/js/bootstrap.js"></script></body>
 </html>
